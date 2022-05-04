ff6e6a7e2fdd4aa93358c5be9f16d14c592e7f10
YermakA <ermak.080@mail.ru>
2022-05-04 16:26:01 +0800

    form moved to the PostForm object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useRef } from 'react';
 import './styles/App.css'
 import PostList from './PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import Postform from './components/PostForm';
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'javascript', body: 'Description' },
@@ -12,42 +11,19 @@ function App() {
     { id: 3, title: 'python', body: 'Description' }]);
 
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'ruby', body: 'Description' },
-    { id: 2, title: 'golang', body: 'Description' },
-    { id: 3, title: 'python', body: 'Description' }]);
 
-  const [title, setTitle] = useState('dsads');
-  const bodyInputRef = useRef();
-
-  const addNewPost = (e) => {
-
-    e.preventDefault();
-    console.log(title);
-    console.log(bodyInputRef.current.value);
-
+  const creatPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
+
+
   return (
     <div className='App' >
-      <form>
-        <MyInput
-          value={title}
-          onChange={e => { setTitle(e.target.value) }}
-          type='text'
-
-          placeholder='название поста'
-        />
-
-        <MyInput
-          ref={bodyInputRef}
-          type='text'
-          placeholder='описание поста'
-        />
-        <MyButton onClick={addNewPost}>create post</MyButton>
-      </form>
+      <Postform create={creatPost} />
       <PostList posts={posts} title='postList1' />
-      <PostList posts={posts2} title='postList2' />
     </div>
   );
 }
+
+
 export default App;
